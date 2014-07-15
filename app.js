62995bc138572eeec00bfaeded50f81602ab63b6
spaceribs <spaceribs@gmail.com>
2014-07-15 18:50:36 -0400

    Static route change.

--- app.js
+++ app.js
@@ -19,18 +19,18 @@ config(["$routeProvider", "$locationProvider", "$compileProvider", function($rou
 	@toc 1.
 	*/
 	$locationProvider.html5Mode(false);		//can"t use this with github pages / if don"t have access to the server
-	
+
 	// var staticPath ="/";
 	var staticPath;
 	// staticPath ="/angular-services/Blobeval/";		//local
-	staticPath ="/";		//nodejs (local)
-	// staticPath ="/Blobeval/";		//gh-pages
+	//staticPath ="/";		//nodejs (local)
+	staticPath ="/blobeval/";		//gh-pages
 	var appPathRoute ="/";
 	var pagesPath =staticPath+"pages/";
-	
-	
+
+
 	$routeProvider.when(appPathRoute+"home", {templateUrl: pagesPath+"home/home.html"});
 
 	$routeProvider.otherwise({redirectTo: appPathRoute+"home"});
-	
+
 }]);
